d83170e7520daa8e7912550f083224a5e8805d52
SOFIA <sofiadamasin@gmail.com>
2024-10-23 11:53:20 -0300

    Create 0101.py

---
+++ listas-tuplas/II/0101.py
@@ -0,0 +1,11 @@
+X = list(map(int,input().split()))[:5]
+Y = list(map(int,input().split()))[:5]
+
+for i in range(len(X)):
+     if X[i] > 1 or Y[i] > 1:
+           print('Valores inválidos!')
+     elif X[i] != Y[i]:
+           print('S')
+     else: 
+           print('N')
+     break
