7d20c622907ea005c7858d36f5cbccb8b5b1bcf5
SOFIA <sofiadamasin@gmail.com>
2024-11-06 12:40:29 -0300

    Update 0011.py

--- dicts/0011.py
+++ dicts/0011.py
@@ -1,24 +1,38 @@
-num_traduçoes = int(input('Número de traduções: '))
+mensagem = {'ingles': 'Merry Christmas', 
+            'portugues': 'Feliz Natal', 
+            'espanhol': 'Feliz Navidad', 
+            'alemao': 'Frohe Weihnachten', 
+            'italiano': 'Buon Natale'}
 
-mensagem = {'ingles': 'Merry Christmas', 'portugues': 'Feliz Natal', 'espanhol':
-            'Feliz Navidad', 'alemao': 'Frohe Weihnachten', 'italiano': 'Buon Natale'}
+traduçoes = int(input('Número de traduções: '))
 
-i = 1
-while i < num_traduçoes:
-    num_crianças = int(input('Número de crianças: '))
-    if num_crianças < 1 and num_crianças > 100:
-        break
-    
+def quantidade_e_informaçoes():
     info = []
-    for i in range(num_crianças):
-        nome = input('Nome: ')
-        lingua = input('Língua: ')
-        info.append(nome)
-        info.append(lingua)
-            
-        print()
-        for idioma in mensagem:
-            if lingua == idioma:
-                print(nome)
-                print(mensagem[idioma])
+    for _ in range(traduçoes):
+        crianças = int(input('Número de crianças: '))
+
+        if crianças < 1 and crianças > 100:
+            break
+        else:
+            for _ in range(crianças):
                 print()
+                nome = input('Nome: ').title()
+                lingua = input('Língua: ').lower()
+                info.append(nome)
+                info.append(lingua)
+
+    return info
+
+def mensagem_crianças(info):
+    print()
+    print('Traduções de "Feliz Natal":')
+    
+    for i in range(0, len(info), 2):
+        nome = info[i]
+        lingua = info[i+1]
+        print()
+        print(nome)
+        print(mensagem.get(lingua, 'Idioma não encontrado.'))
+
+info = quantidade_e_informaçoes()
+mensagem_crianças(info)
