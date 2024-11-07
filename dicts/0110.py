d7c1253ebac2887c4f735b7953102ee4094e6a68
SOFIA <sofiadamasin@gmail.com>
2024-11-07 12:35:09 -0300

    Update 0110.py

--- dicts/0110.py
+++ dicts/0110.py
@@ -1,34 +1,40 @@
 infos = []
+cadastro = {}
+clt = {}
 
-while True:
-    cadastro = {}
-    cadastro['nome'] = input('Nome: ').capitalize()
-    ano = int(input('Ano de nascimento: '))
-    cadastro['idade'] = 2024 - ano 
-    cadastro['CLT'] = int(input('Carteira de trabalho: '))
-
-    infos.append(cadastro)
+def informacoes_clt():
     
-    clt = []
-    if cadastro['CLT'] != 0:
-        ano_contrataçao = int(input('Ano de contratação: '))
-        salario = float(input('Salário: '))
-        aposentadoria = ano_contrataçao - ano + 35
-        
-        clt.append(ano_contrataçao)
-        clt.append(salario)
-        clt.append(aposentadoria)
+    if cadastro['CLT'] != 'Não':
+        clt['ano_contrataçao'] = int(input('Ano de contratação: '))
+        clt['salario'] = float(input('Salário: R$ '))
+        clt['aposentadoria'] = clt['ano_contrataçao'] - ano + 35
         
     cadastro['informações'] = clt
 
+while True:
+
+    cadastro['pessoa'] = input('Nome: ').title()
+    ano = int(input('Ano de nascimento: '))
+    cadastro['idade'] = 2024 - ano 
+    cadastro['CLT'] = input('Carteira de trabalho (Sim/Não): ').capitalize()
+
+    infos.append(cadastro)
+
     print()
-    add_informacoes = input('Deseja adicionar informações? ').upper()
-    if add_informacoes != 'S':
+    informacoes_clt()
+
+    print()
+    print('Nome:',cadastro.get('pessoa'))
+    print('Idade:',cadastro.get('idade'))
+    print('CLT:',cadastro.get('CLT'))
+    print()
+    print('INFORMAÇÕES DA CLT')
+    print('Ano de contratação:',clt.get('ano_contrataçao'))
+    print('Salário: R$',clt.get('salario'))
+    print('Idade de aposentadoria:',clt.get('aposentadoria'),'anos')
+
+    print()
+    add_info = input('Deseja adicionar informações? ').upper()
+    if add_info != 'S':
         break
     print()
-
-print()
-print('Nome:',cadastro.get('nome'))
-print('Idade:',cadastro.get('idade'))
-print('CLT:',cadastro.get('CLT'))
-####print('Informações de CLT:',cadastro.get('informações'))
