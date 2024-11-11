e9b0760e8fa0fd04ebe3136181c8a1e5a6699d7a
SOFIA <sofiadamasin@gmail.com>
2024-11-11 13:24:15 -0300

    Update 0010.py

--- dicts/0010.py
+++ dicts/0010.py
@@ -1,37 +1,34 @@
-viagens = int(input('Número de viagens ao mercado: '))
+def verificar_viagens():
+    return int(input('Número de viagens ao mercado: '))
+
+def verificar_produtos():
+    return int(input('Quantidade de produtos disponíveis: '))
+        
+def escolher_produtos():
+    return int(input('Quantos produtos deseja comprar? '))
+
+def registrar_produtos():
+    prods_e_preços['produto'] = input(f'{i+1}º produto: ').lower()
+    prods_e_preços['preço'] = float(input('Preço: R$ '))
+
+def registrar_compras():
+    prods_e_quantidade['produto'] = input(f'{i+1}º produto: ').lower()
+    prods_e_quantidade['quantidade'] = int(input('Quantidade: '))
+
+viagens = verificar_viagens()
 prods_e_preços = {}
 prods_e_quantidade = {}
 
-def verificar_produtos():
-    print()
-    for _ in range(viagens):
-        qtd_produtos = int(input('Quantidade de produtos disponíveis: '))
-        print()
-        for i in range(qtd_produtos):
-            prods_e_preços['produto'] = input(f'{i+1}º produto e preço: ').lower()
-            try:
-                prods_e_preços['preço'] = float(prods_e_preços['produto'])
-            except ValueError:
-                prods_e_preços['preço'] = prods_e_preços['produto']
+while viagens != 0:
+    qtd_produtos = verificar_produtos()
 
-        escolher_produtos()
-        
-def escolher_produtos():
-    print()
-    qtd_compras = int(input('Quantos produtos deseja comprar? '))
+    for i in range(qtd_produtos):
+        registrar_produtos()
+    
+    qtd_compras = escolher_produtos()
     for i in range(qtd_compras):
-        prods_e_quantidade['produto'] = input(f'{i+1}º produto e quantidade: ').lower()
-        try:
-            prods_e_quantidade['quantidade'] = int(prods_e_quantidade['produto'])
-        except ValueError:
-            prods_e_quantidade['quantidade'] = prods_e_quantidade['produto']
+        registrar_compras()
 
-verificar_produtos()
-
-resultado = {}
-for produto in prods_e_preços:
-    if produto in prods_e_quantidade:
-        resultado[produto] = prods_e_preços[produto] * prods_e_quantidade[produto]
-
-print(resultado)
-print(f'Valor total: {sum(resultado)}')
+    viagens -= 1
+    
+print(prods_e_preços)
