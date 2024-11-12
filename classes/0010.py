4df02f07cac9579c79b10c4ef2ed5da8b1101f28
SOFIA <sofiadamasin@gmail.com>
2024-11-11 21:53:13 -0300

    Create 0010.py

---
+++ classes/0010.py
@@ -0,0 +1,31 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+
+    def atualizar_nome(self, nome):
+        novo_nome = input("Digite o novo nome: ")
+        self.nome = novo_nome
+        
+    def atualizar_idade(self, idade):
+        nova_idade = input("Digite a nova idade: ")
+        self.idade = int(nova_idade)
+
+    def atualizar_peso(self, peso):
+        novo_peso = input("Digite o novo peso: ")
+        self.peso = float(novo_peso)
+        
+while True:
+    infos = Pessoa(input('Nome: '), int(input('Idade: ')), float(input('Peso: '))) 
+    
+    att_infos = input('Deseja atualizar as informações? ').capitalize()
+    if att_infos == 'Sim':
+        infos.atualizar_nome(nome)
+        infos.atualizar_idade(idade)
+        infos.atualizar_peso(peso)
+        
+    add_infos = input('Deseja adicionar informações? ').capitalize()
+    if add_infos != 'Sim':
+        break
+
