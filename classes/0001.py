6f1cb5bc2db743da2da042db66fb83ef6b75dcc7
SOFIA <sofiadamasin@gmail.com>
2024-11-12 12:43:10 -0300

    Update 0001.py

--- classes/0001.py
+++ classes/0001.py
@@ -1,8 +1,31 @@
-class Exponencial:
-    def inserir_numero():
-        return int(input('Insira um número: '))
+class Expoente3:
+    def __init__(self, numero):
+        self.numero = numero
+        self.cubo = self.elevar_ao_cubo()
+
+    def elevar_ao_cubo(self):
+        return self.numero**3
     
-    def num_ao_cubo():
-        cubo = n**3
-        return cubo
+    def __str__(self):
+        return f'CUBO DE {self.numero}: {self.cubo}'
+
+def obter_numero():
+    entrada = input('NÚMERO: ').lower()
+    if entrada == 'parar':
+        return 'parar'
+    return int(entrada)
+
+def main():
+    while True:
+        entrada = obter_numero()
+        if entrada == 'parar':
+            print('Encerrando o programa...')
+            break
+            
+        potenciaçao = Expoente3(entrada)
+        print(potenciaçao)
+        print()
+
+if __name__ == '__main__':
+    main()
     
